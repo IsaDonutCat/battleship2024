675b531ed4c0b4e51031c6c8b2cf811443e5cdd7
Isabella Wang <wangisa26@7hills.org>
2023-10-02 08:42:45 -0400

    started placeShip

--- Board.java
+++ Board.java
@@ -16,7 +16,7 @@ public class Board
         for (int a = 0; a < numRows; a++)
         {
             for(int b = 0; b < numCols; b++)
-                gameBoard[a][b] = 2;
+                gameBoard[a][b] = 0;
         }
     }
 
@@ -32,6 +32,30 @@ public class Board
                         System.out.print(gameBoard[a][b]);
                 }
             System.out.print("\n");  //new line char
+        }
+    }
+
+    public void placeShip()
+    {   
+        Scanner shipInput = new Scanner(System.in);
+
+        
+        String ans = "";
+        int r,c,orient;
+
+        while (ans != "Y")
+        {
+            System.out.println("XXX\n XXX");
+            System.out.println("Place your armored carrier Uppercase Letter/Number/Degrees will be the top left corner:");
+
+            ans = shipInput.nextLine(); 
+            r = (int) ans.charAt(0) - 65; // ascii value of A
+            c = (int) str.substring(ans.indexOf("/") + 1, ans.lastIndexOf("/")) - 1; // the plus one is we don't want, the minus accounts for diff between human and computer counting
+            orient = (int) Integer.valueOf(ans.substring(ans.lastIndexOf("/")+1,ans.length())); //setting values;
+
+            if (orient % 90 != 0 )
+                System.out.println("Impossible orientation. Orientation must be in right angles");
+            else if (r >= numRows || c >= numCols)
         }
     }
 }
