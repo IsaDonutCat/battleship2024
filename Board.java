776a8cc5cdb9eb74ad2cba4b0adbd9b8f3762714
Isabella Wang <wangisa26@7hills.org>
2023-10-02 12:45:50 -0400

    fully restarting lets go (/s)

--- Board.java
+++ Board.java
@@ -1,110 +1,19 @@
-import java.util.Scanner;
-
 public class Board
 {
-    int numRows =10;
-    int numCols =10;
+    int numRow, numCol;
+    char grid[][];
 
-    //int[][] gameBoard = new int[numRows][numCols];
-    int[][] gameBoard;
- 
-    public Board (int c, int r)
+    public Board (int r, int c)
     {
-        numRows = r;
-        numCols = c;
+        numRow = r;
+        numCol = c; // reduces need to continually pass on variables
 
-        int[][] gameBoard = new int[r][c];
+        grid = new char[r][c];
 
-        for (int a = 0; a < numRows; a++)
+        for (int i = 0; i < r; i++)
         {
-            for(int b = 0; b < numCols; b++)
-                gameBoard[a][b] = 0;
+            for (int j = 0; j < c; j++)
+                grid[i][j] = ' '; //nulls stuff out
         }
     }
-
-
-
-    public void printBoard()
-    {   
-
-        for (int a = 0; a <= numRows-1; a++)
-        {
-                for (int b = 0; b <= numCols-1; b++)
-                {
-                        System.out.print(gameBoard[a][b]);
-                }
-            System.out.print("\n");  //new line char
-        }
-    }
-
-    public void placeShip(String shipName, int shipSize)
-    {   
-        Scanner shipInput = new Scanner(System.in);
-
-        
-        String ans = "";
-        int r,c,orient,loop;
-
-        while (ans != "Y")
-        {
-            System.out.println("XXX\n XXX");
-            System.out.println("Place your " +  shipName + ". Uppercase Letter/Number/Degrees will be the left corner:");
-
-            ans = shipInput.nextLine(); 
-            r = (int) ans.charAt(0) - 65; // ascii value of A
-            c = Integer.valueOf(ans.substring(ans.indexOf("/") + 1, ans.lastIndexOf("/") )) - 1; // the plus one is we don't want, the minus accounts for diff between human and computer counting
-            orient = Integer.valueOf(ans.substring(ans.lastIndexOf("/")+1,ans.length())); //setting values;
-            loop = 0;
-
-            if (orient % 90 != 0 )
-                System.out.println("Impossible orientation. Orientation must be in right angles");
-            else
-            {
-                if ((orient / 90) % 2 == 0)
-                {
-                    if (c > numCols || r + shipSize > numRows)
-                        System.out.print("Ship is off the board. The number of rows is " + numRows + " and the number of columns is " + numCols);
-                    else
-                    {
-                        while (loop < shipSize)
-                        {
-                            if (gameBoard[r+loop][c] == 0)
-                                gameBoard[r + loop][c] = 1;
-                            else
-                            {
-                                System.out.print("A ship is already there.");
-                                break;
-                            }
-                            loop++;
-                        }
-                        loop = 0;
-                    }
-                }
-                else
-                {
-                    if (c + shipSize > numCols || r > numRows)
-                        System.out.print("Ship is off the board. The number of rows is " + numRows + " and the number of columns is " + numCols);
-                    else
-                    {
-                        while (loop < shipSize)
-                        {
-                            if (gameBoard[r][c + loop] == 0)
-                                gameBoard[r][c + loop] = 1;
-                            else
-                            {
-                                System.out.print("A ship is already there.");
-                                break;
-                            }
-                            loop++;
-                        }
-                        loop = 0;
-                    }
-                }
-            }
-
-        }//end of while loop
-        printBoard();
-        shipInput.close();
-    }//end of printShip method
-
-}// end o class file
+}
--- tester2.java
+++ tester2.java
@@ -3,6 +3,30 @@ import java.util.Scanner; // input
 
 public class tester2 
 {
-    
-    Board gameBoard = new Board()
+    public static void main (String[] args)
+    {
+        int numRow, numCol;
+        boolean random; 
+        Scanner testerInput = new Scanner(System.in);
+
+        System.out.print("What type of game? (fast/regular)");
+        String ans = testerInput.nextLine();
+
+        if (ans.toLowerCase().equals("regular"))
+        {
+            numRow = 10;
+            numCol = 10;
+            random = false;
+        }    
+        else if (ans.toLowerCase().equals("fast"))
+        {
+            numRow = 8;
+            numCol = 8;
+            random = true;
+        }
+
+        Board gameBoard = new Board(numRow, numCol);
+
+        
+    }
 }
