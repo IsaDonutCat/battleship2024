bee36ed7a7ac07a0b448e5f4a86858bfbd4f6548
Isabella Wang <wangisa26@7hills.org>
2023-10-02 12:50:30 -0400

    recreated printBoard

--- Board.java
+++ Board.java
@@ -10,10 +10,42 @@ public class Board
 
         grid = new char[r][c];
 
-        for (int i = 0; i < r; i++)
+        for (int i = 0; i < numRow; i++)
         {
-            for (int j = 0; j < c; j++)
+            for (int j = 0; j < numCol; j++)
                 grid[i][j] = ' '; //nulls stuff out
         }
-    }
-}
+
+        printBoard();
+    }//closes constructor
+
+    public void printBoard() // prints board. should run everytime the board is changed so user knows what's going on
+    {
+        for (int i = 0; i < numRow; i++)
+        {
+
+            for (int j = 0; j < numRow; j++)
+            {
+                System.out.print(grid[i][j]);
+                if (j != numRow - 1) // if not on the last item
+                {
+                    System.out.print("|");
+                }
+            }
+            System.out.println();
+
+            
+            if (i != numRow - 1)
+            {
+                for (int j = 0; j < numRow; j++)
+                {
+                    System.out.print("-");
+                }
+            } // adds line spacing
+            System.out.println();
+        }
+
+        return;
+    }//closes printboard
+
+}//closes class
